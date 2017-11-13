592991028e985d47cca2eb2193d010078653f036
maxwilliamchao <maxwilliamchao@gmail.com>
2017-11-13 08:59:31 -0800

    pausing gifs

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -38,6 +38,21 @@ $.ajax({
 
 
 
+
+
+var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+
+
       // //Starts my count at 0
       // var giphyCount = 0;
 
